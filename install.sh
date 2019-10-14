e768802ce888ac879d877cd012e59ae1dc02d974
Francois Smit <francois@irisns.com>
2019-10-14 13:10:07 +0200

    Change install to fix process ownership in systemd script

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ InstallPath="/opt/SmittenBooth"
 sudo mkdir -p $InstallPath
 sudo chown -R $USER:$USER $InstallPath
 cp -r $BasePath/includes $BasePath/photobooth.py $InstallPath/
-sudo cp $BasePath/includes/systemd/photobooth.service /lib/systemd/system/photobooth.service
+sed -i 's/{user}/'"$USER"'/g' $InstallPath/includes/systemd/photobooth.service
+sudo cp $InstallPath/includes/systemd/photobooth.service /lib/systemd/system/photobooth.service
 sudo systemctl enable photobooth.service
 
 echo "To Start use command: sudo systemctl start photobooth.service"
